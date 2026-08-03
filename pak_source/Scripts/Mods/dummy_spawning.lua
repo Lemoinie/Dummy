3e3fc852b90b4e72300185bdd94bcf64d4207098
TrashCan <phuthuytinhyeu1234678@gmail.com>
2026-08-03 23:01:25 +0700

    Revert invalid serialization properties from Dummy spawn

--- pak_source/Scripts/Mods/dummy_spawning.lua
+++ pak_source/Scripts/Mods/dummy_spawning.lua
@@ -327,8 +327,6 @@ function DummySpawner:Spawn()
             bWH_PerceptorObject     = true,
             bWH_PerceptibleObject   = true,
             bInvulnerable           = true,
-            bSaved_by_game          = true,
-            bSerialize              = true,
         }
     })
 
